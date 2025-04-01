0f7554f7e7ec15fb9f2e9942942ebe92ff2cf82c
rlowden22 <lowden.r@northeastern.edu>
2025-04-01 12:37:48 -0700

    updated IView and IController

--- src/main/java/student/view/IView.java
+++ src/main/java/student/view/IView.java
@@ -4,28 +4,118 @@ import student.model.Movie;
 import java.util.List;
 import java.awt.event.ActionListener;
 
-public interface IView {void setVisible(boolean visible);  //visibility
+public interface IView {
+     /**
+     * Sets the visibility of the GUI window.
+     *
+     * @param visible true to show the view, false to hide it
+     */
+    void setVisible(boolean visible);  
     
 
-    // view movie data
-    void viewMovieCollection(List<Movie> movies);    //shows full movie collection
-    void viewMovieList(List<Movie> movies);  //shows updated movieList
-    void showErrorMessage(String errorMessage); //display error message to user
+    /**
+     * Displays the full movie collection or filtered once user applies filter or sort.
+     *
+     * @param movies the list of all movies retrieved from the API or data source
+     */
+    void viewMovieCollection(List<Movie> movies);
+
+    /**
+     * Displays the user's personalized movie list in the view once they add or remove movies. 
+     *
+     * @param movies the list of movies the user has added to their saved list
+     */
+    void viewMovieList(List<Movie> movies);  
+
+    /**
+     * Displays an error message to the user 
+     *
+     * @param errorMessage the message to be shown
+     */
+    void showErrorMessage(String errorMessage); 
+
+       /**
+     * Displays an help message to the user 
+     *
+     * @param helpMessage the message to be shown
+     */
+    void showHelpMessage(String helpMessage); 
 
     // user input getters
-    String getSearchQuery(); //search from text field
-    String getSelectedFilter(); //get the selected filter (average rating, genre, year, cast member, etc )
+
+    /**
+     * Returns the current text input from the search/filter field.
+     *
+     * @return the text entered by the user
+     */
+    String getSearchQuery(); 
+    /**
+     * Returns the selected filter parameter (e.g., average rating, genre, year).
+     *
+     * @return the filter field selected by the user
+     */
+    String getSelectedFilter(); 
+
+    /**
+     * Returns the selected comparison operator (e.g., ==, >=, <=).
+     *
+     * @return the operator selected by the user
+     */
     String getSelectedOperator(); // get the operator selected to apply to the search (>, <, ==. !=, ~= etc)
+
+     /**
+     * Returns the sort direction.
+     *
+     * @return true for ascending sort, false for descending
+     */
     boolean getSort(); // sort the filtered collection ascending or decsending
 
 
     //event listenters to connect view to controller
     //waiting for user action (click button, typing in field, dropdown menu)
+
+      /**
+     * Registers a listener for the search action.
+     *
+     * @param listener the ActionListener to be notified when the search button is clicked
+     */
     void addSearchListener(ActionListener listener);
+
+     /**
+     * Registers a listener for the filter action.
+     *
+     * @param listener the ActionListener to be notified when the filter is applied
+     */
     void addFilterListener(ActionListener listener);
+
+    /**
+     * Registers a listener for the sort action.
+     *
+     * @param listener the ActionListener to be notified when sorting is selected
+     */
     void addSortListener(ActionListener listener);
+
+
+      /**
+     * Registers a listener for the "Add to List" button.
+     *
+     * @param listener the ActionListener to be notified when a movie is added to the user's list
+     */
+
     void addAddMovieListener(ActionListener listener);
+
+      /**
+     * Registers a listener for the "Remove from List" button.
+     *
+     * @param listener the ActionListener to be notified when a movie is removed from the user's list
+     */
     void addRemoveMovieListener(ActionListener listener);
+
+     /**
+     * Registers a listener for the "Save to File" action.
+     *
+     * @param listener the ActionListener to be notified when the save button is clicked
+     */
     void addSaveListener(ActionListener listener);
 }
 
